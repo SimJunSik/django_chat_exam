9451e064b3f3205782e97b44bc5e5f127bcc21f6
SimJunSik <wnstlr24@naver.com>
2019-11-04 02:20:33 +0900

    1911040220

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,9 +5,9 @@ from .models import *
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        print(self.scope['url_route'])
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        # self.user_name = self.scope['url_route']['kwargs']['user_name']
-        self.user_name = self.scope['query_string'].decode('utf8')
+        self.user_name = self.scope['url_route']['kwargs']['user_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         count = getattr(self.channel_layer, self.room_group_name, 0)
